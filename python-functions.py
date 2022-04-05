92fa5d79e3acead2649d693b975ec46c032f14ac
byamane <byamane21@gmail.com>
2022-04-05 10:16:45 -0500

    complete challenge 2

--- python-functions.py
+++ python-functions.py
@@ -6,5 +6,14 @@ def sum_to(n):
     sum += i
   print(sum)
 
-sum_to(6)
-sum_to(10)
+sum_to(6)  # returns 21
+sum_to(10) # returns 55
+
+# Challenge 2
+
+def largest(list):
+  max_num = max(list)
+  print(max_num)
+
+largest([1, 2, 3, 4, 0])  # returns 4
+largest([10, 4, 2, 231, 91, 54])  # returns 231
